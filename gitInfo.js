49af6a179017f3151cb2b0cbe840dabf3b6dc07c
Sonalee Bhattacharyya <sonaleebhattacharyya@gmail.com>
2022-12-15 22:04:03 -0600

    w1assessment

---
+++ gitInfo.js
@@ -0,0 +1,72 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+//CODE HERE
+let gitDefinition = "git is a way to track changes in files and have multiple people work on the same files"
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+//CODE HERE
+let gitHubDefinition = "github is a place to store the repositories containing the files. It updates when you make a change in the file on your computer"
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+//CODE HERE
+let gitInitDefinition = "git init initializes git to start tracking changes, which you can then push to github"
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+//CODE HERE
+let gitCloneDefinition = 'allows you to clone or copy a repository to put it in a new location.'
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+//CODE HERE
+let gitStatusDefinition = "how to see what files are being tracked by git"
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+//CODE HERE
+let gitAdddefinition = "to add changes in files to git"
+let gitAddCode = "git add ."
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitcommitdefinition = "commits to the changes you made"
+let gitCommitCode = "git commit -m'type a message here' "
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = "it pushes files and changes to your online github repository"
---
+++ orchard.js
@@ -0,0 +1,204 @@
+///////////////////
+// Apple Orchard //
+///////////////////
+
+/*
+    The data below has been collected over the 
+    last week at an apple orchard. You will be 
+    using your coding skills to gain insights 
+    into its daily operations.
+
+    Below are three arrays each containing 7
+    numbers. You will use them and the price
+    variables to complete the problems in 
+    this file.
+
+    The indexes of the arrays correspond with a 
+    day of the week. The associated number value 
+    represents the number of acres that were picked 
+    that day.
+
+    The first array is named fujiAcres. Notice 
+    that the first item in this array is 2, 
+    meaning that 2 acres of Fuji apples were 
+    picked on Monday of this week.
+
+    The prices are per pound and are written
+    in cents. 
+*/
+
+const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
+const galaAcres = [5, 2, 4, 3, 6, 2, 4]
+const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
+
+const fujiPrice = .89 
+const galaPrice = .64
+const pinkPrice = .55
+
+// PROBLEM 1
+
+/*
+    Using for loop, calculate the total number
+    of acres picked for the entire week.
+    Save the number to a variable called 
+    `totalAcres`.
+    Log `totalAcres` to the console.
+*/
+// this will add each ith element of the three arrays to the total sum. I guess it changed 
+// the original arrays and I am not sure how to fix that. I just wanted it to change sum. 
+// I probably won't have time to figure out how to fix it before this is due. 
+var totalAcres = 0;
+// CODE HERE
+for (i = 0; i<= fujiAcres.length -1; i++) {
+    sum = fujiAcres[i] += galaAcres[i] += pinkAcres[i]
+    totalAcres += sum
+}
+console.log(totalAcres)
+
+// PROBLEM 2
+
+/*
+    Using `totalAcres`, calculate the average 
+    number of acres picked per day. 
+
+    Save the answer to a variable called 
+    `averageDailyAcres`. 
+
+    Log `averageDailyAcres` to the console.
+*/
+
+// CODE HERE
+var averageDailyAcres = totalAcres / 7
+console.log(averageDailyAcres)
+
+// PROBLEM 3
+/*
+    We have provided 2 variables below. 
+    The `acresLeft` variable is the number 
+    of acres that still have apples left. 
+    The `days` variable represents how 
+    many more days of work are left. It's 
+    initialized at 0 because we're going 
+    to be using it as a counter.
+    Write a while loop that will continue to run
+    while `acresLeft` is above 0. 
+    On each iteration of the loop:
+    - add 1 to the `days` variable
+    - subtract your daily average from the number of acres left
+    Outside the loop, log `days` to the console.
+    Note: This is not the most efficient way to
+    calculate this number. But! It is a great
+    way to get a whole number without using 
+    any Math methods. 
+
+*/
+
+let acresLeft = 174;
+let days = 0;
+
+// CODE HERE
+while( acresLeft > 0) {
+    days += 1,
+    acresLeft -= averageDailyAcres
+}
+console.log("problem3")
+console.log(days)
+
+// PROBLEM 4
+
+/*
+    Your next task is to create 3 arrays
+    that list the daily amount of apples 
+    picked, in tons, for each variety. 
+    Each acre yields 6.5 tons of apples
+    Use the variables below to store 
+    your new arrays. Make sure that you
+    don't modify the original arrays 
+    on lines 36 - 38.
+    Log each of your arrays to the console.
+    Hint: you can use the slice method 
+    to make copies of the arrays and 
+    then loop the copies. You could also
+    make empty arrays, loop the old ones
+    and use the push method to add new 
+    values to the new arrays.
+*/
+
+// CODE HERE 
+const fujiA = [2, 3, 3, 2, 2, 2, 1]
+const galaA = [5, 2, 4, 3, 6, 2, 4]
+const pinkA = [1, 5, 4, 2, 1, 5, 4]
+
+let fujitons=fujiA.map(value => value*6.5); 
+console.log("fujitons ="+fujitons)
+
+let galatons=galaA.map(value => value*6.5); 
+console.log("galatons ="+galatons)
+
+let pinktons=pinkA.map(value => value*6.5); 
+console.log("pinktons ="+pinktons)
+
+// PROBLEM 5
+/*
+    Next, calculate the total number of 
+    pounds picked per variety.
+    You'll need to add up the tons per
+    each variety and convert the number 
+    into pounds -- store that number in
+    the variables given below. 
+    Log each of the values to the console.
+    Hint: there are 2000 pounds in a ton.
+*/
+
+// CODE HERE  
+// we will use the .reduce method to find the sum.
+let findPounds = (arr) => {
+    return 2000* arr.reduce(function (x,y) {
+        return x + y;
+    }, 0);
+}
+let fujipounds= findPounds(fujitons)
+let galapounds= findPounds(galatons)
+let pinkpounds= findPounds(pinktons)
+console.log("total fuji pounds is "+fujipounds)
+console.log("total gala pounds is "+galapounds)
+console.log("total pink pounds is "+pinkpounds)
+
+// PROBLEM 6
+/*
+    Now that you know the total pounds
+    per variety, use the prices given 
+    at the beginning of this file to 
+    figure out how much you'll make 
+    from selling each type of apple. 
+    The prices are per pound and are 
+    written in cents. 
+    Log each of the profits to the 
+    console. 
+*/
+
+// const fujiPrice = .89 
+// const galaPrice = .64
+// const pinkPrice = .55
+
+// CODE HERE
+
+let fujiProfit = fujiPrice*fujipounds
+let galaProfit = galaPrice*galapounds
+let pinkProfit = pinkPrice*pinkpounds
+console.log(fujiProfit)
+console.log(galaProfit)
+console.log(pinkProfit)
+
+
+// PROBLEM 7
+
+/*
+    Add up all your profits and save 
+    the number to a variable called 
+    `totalProfit`.
+
+    Log `totalProfit` to the console.
+*/
+
+// CODE HERE
